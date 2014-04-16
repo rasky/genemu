772a4588ef91f37279e1db3dbf8beeb0cf16a1fa
Giovanni Bajo <rasky@develer.com>
2014-04-17 01:34:53 +0200

    SOUND: bump frequency used for YM2612 (trying to fix bugs)

--- hw.h
+++ hw.h
@@ -15,7 +15,7 @@ int hw_poll(void);
 void hw_beginframe(uint8_t **screen, int *pitch);
 void hw_endframe();
 
-#define HW_AUDIO_FREQ         44100
+#define HW_AUDIO_FREQ         53267
 #define HW_AUDIO_NUMSAMPLES   (HW_AUDIO_FREQ / 60)
 
 void hw_beginaudio(int16_t **buf);
