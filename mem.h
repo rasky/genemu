e6e7d6e95b8dd0295a6edbbde55bdfcff00e7e94
Giovanni Bajo <rasky@develer.com>
2014-04-21 01:13:03 +0200

    Remove unused variables

--- mem.h
+++ mem.h
@@ -1,7 +1,4 @@
 
-extern int Z80_BUSREQ;
-extern int Z80_RESET;
-
 #define BIT(v, idx)       (((v) >> (idx)) & 1)
 #define BITS(v, idx, n)   (((v) >> (idx)) & ((1<<(n))-1))
 #define FETCH16(mem)      (((mem)[0] << 8) | (mem)[1])
