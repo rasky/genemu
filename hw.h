ebf3cdb061d8d9e9e24b2ddf614ce5efa33bd007
Giovanni Bajo <rasky@develer.com>
2014-04-30 19:54:06 +0200

    Switch back to NTSC timing

--- hw.h
+++ hw.h
@@ -18,7 +18,7 @@ void hw_beginframe(uint8_t **screen, int *pitch);
 void hw_endframe();
 
 #define HW_AUDIO_FREQ         53267
-#define HW_AUDIO_NUMSAMPLES   (HW_AUDIO_FREQ / 50)
+#define HW_AUDIO_NUMSAMPLES   (HW_AUDIO_FREQ / 60)
 
 void hw_beginaudio(int16_t **buf);
 void hw_endaudio(void);
