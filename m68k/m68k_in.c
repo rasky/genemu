73921d2595fb33756c3b6a6f325b69f5e0b494d6
Giovanni Bajo <rasky@develer.com>
2014-05-03 02:54:43 +0200

    M68K: missing removal of m68ki_remaining_cycles

--- m68k/m68k_in.c
+++ m68k/m68k_in.c
@@ -9237,7 +9237,7 @@ M68KMAKE_OP(stop, 0, ., .)
 		m68ki_trace_t0();			   /* auto-disable (see m68kcpu.h) */
 		CPU_STOPPED |= STOP_LEVEL_STOP;
 		m68ki_set_sr(new_sr);
-		m68ki_remaining_cycles = 0;
+		USE_ALL_CYCLES();
 		return;
 	}
 	m68ki_exception_privilege_violation();
